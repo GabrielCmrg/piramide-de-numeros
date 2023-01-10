af86c367c9b57cafdd0568d5ab5f94dbf427fe10
Gabriel C <gabriel.camargo96@hotmail.com.br>
2023-01-10 14:49:14 -0300

    feat: prints a piramid with the input

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Quantas linhas você quer que sua piramide tenha?");
+        Scanner scan = new Scanner(System.in);
+        int lines = scan.nextInt();
+        scan.close();
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 }
